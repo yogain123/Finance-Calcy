9c874518cef382b3cdfc32e40dfd8ae6352132e1
yogain19 <yogendrasaxena56@gmail.com>
2020-06-26 17:01:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { localeformat } from "./utils";
 
 function App() {
+  const [bankAccount, setBankAccount] = useState(0);
+  const [groww, setGroww] = useState(0);
+  const [fd, setFd] = useState(0);
+  const [pf, setPf] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const calculate = (event) => {
+    event.preventDefault();
+    console.log({ bankAccount, groww, fd, pf });
+    const formattedResult = localeformat(String(bankAccount + groww + fd + pf));
+    setResult(formattedResult + " Rs");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container App">
+      <h3>Your Financial Calculator</h3>
+      <form>
+        <div className="form-group">
+          <label htmlFor="groww">GROWW</label>
+          <input
+            onChange={(event) => setGroww(+event.target.value)}
+            type="text"
+            className="form-control"
+            id="groww"
+            placeholder="Enter groww amount"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="saving_bank_account">SAVING BANK ACCOUNT</label>
+          <input
+            onChange={(event) => setBankAccount(+event.target.value)}
+            type="text"
+            className="form-control"
+            id="saving_bank_account"
+            placeholder="Enter Saving Bank Account"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="fd">FD</label>
+          <input
+            onChange={(event) => setFd(+event.target.value)}
+            type="text"
+            className="form-control"
+            id="fd"
+            placeholder="Enter FD"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="PF">PF</label>
+          <input
+            onChange={(event) => setPf(+event.target.value)}
+            type="text"
+            className="form-control"
+            id="PF"
+            placeholder="Enter PF"
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={(event) => calculate(event)}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+        <hr />
+        <h3>{result}</h3>
+      </form>
     </div>
   );
 }
